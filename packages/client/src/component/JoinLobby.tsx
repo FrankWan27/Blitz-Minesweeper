2b5139a2523bf6dd40670dfa512847dd4c0ec1dc
Frank Wan <frankwan27@gmail.com>
2023-04-01 05:36:25 -0700

    update p orts

--- packages/client/src/component/JoinLobby.tsx
+++ packages/client/src/component/JoinLobby.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import SocketManager from './websocket/SocketManager';
 
 const JoinLobby = () => {
-  //const sm = new SocketManager();
+  const sm = new SocketManager();
   
   return (
     <div><form className='input'>
         <input type="input" className="inputBox" placeholder="Enter LobbyID"/>
         
     </form>
-    <button onClick={() => {}}>Join</button></div>
+    <button onClick={() => sm.ping()}>Join</button></div>
   )
 }
 
