d05f3c4f2b12764da208d8a28315a57623caeb24
AlexW <wongalex1978@gmail.com>
2023-04-05 03:04:07 -0700

    Fix Tile Size

--- packages/client/src/component/Gameboard.tsx
+++ packages/client/src/component/Gameboard.tsx
@@ -6,65 +6,67 @@ import socketManager from "./websocket/SocketManager";
 
 const sm = socketManager;
 const Tile: React.FC<TileProps> = (props) => {
-    const getText = () => {
-        switch (props.state) {
-            case 'bomb':
-              return '💣';
-            case 'flag':
-              return '🚩';
-            case 'hidden':
-            case 'blank':
-              return ' ';
-            default:
-              return props.state;
-        }
-    }
-    const tileClick = () => {
-        console.log("clicked")
-        if (props.state != 'hidden') {
-            return;
-        }
-        sm.move(props.x, props.y);
-        console.log("clicked and hidden")
-    }
+	const getText = () => {
+		switch (props.state) {
+			case 'bomb':
+				return '💣';
+			case 'flag':
+				return '🚩';
+			case 'hidden':
+			case 'blank':
+				return ' ';
+			default:
+				return props.state;
+		}
+	}
+
+	const tileClick = () => {
+		console.log("clicked")
+		if (props.state != 'hidden') {
+			return;
+		}
+		sm.move(props.x, props.y);
+		console.log("clicked and hidden")
+	}
     
-    return (
-    <td style={{width: '30px', height: '30px'}}>
-        <div 
-        className={'tile' + props.state == 'hidden'? ' hidden' : ' revealed'}
-        onClick={tileClick}>
-            {getText()}
-        </div>
-    </td>
-    )
+	return (
+		<td>
+			<div 
+				style={{width: `${60 / props.width}vw`}}
+				className={`tile ${props.state === 'hidden' ? 'hidden' : 'revealed'}`}
+				onClick={tileClick} >
+					{getText()}
+			</div>
+		</td>
+	)
 }
 
 interface GameboardProps {
-    board: TileState[][],
-    width : number, 
-    height : number,
+	board: TileState[][],
+	width : number, 
+	height : number,
 }
 
 interface TileProps {
-    state: TileState,
-    x : number, 
-    y : number
+	state: TileState,
+	width: number,
+	x : number, 
+	y : number
 }
 
-
-
 export const Gameboard: React.FC<GameboardProps> = (props) => {
-    const grid = [];
-    
-    for (let y = 0; y < props.height; y++) {
-        const row = [];
-        for (let x = 0; x < props.width; x++) {
-            row.push(<Tile state={props.board[x][y]} x={x} y={y} key={`${x}-${y}`} />);
-        }
-        grid.push(<tr key={y}>{row}</tr>);
-    }
+	const grid = [];
+	
+	for (let y = 0; y < props.height; y++) {
+		const row = [];
+		for (let x = 0; x < props.width; x++) {
+			row.push(<Tile state={props.board[x][y]} width={props.width} x={x} y={y} key={`${x}-${y}`} />);
+		}
 
-    return <table>{grid}</table>;
+		grid.push(<tr key={y}>{row}</tr>);
+	}
+
+	return <table>{grid}</table>;
 }
 
 export default Gameboard;
